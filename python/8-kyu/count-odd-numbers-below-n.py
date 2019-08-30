26ee693df136a9d3a039b8fdabf3efc9e175501b
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-30 12:58:24 -0500

    Refactor solution

--- python/8-kyu/count-odd-numbers-below-n.py
+++ python/8-kyu/count-odd-numbers-below-n.py
@@ -20,10 +20,7 @@ oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
 """
 
 def odd_count(n):
-  sum = []
-  for x in range(n):
-    if (x % 2 != 0):
-      sum.append(x)
-  return len(sum)
+  import math
+  return math.floor(n / 2)
 
 odd_count(15)
