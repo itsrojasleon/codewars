9c00193c72704286bf7c8b55fa9264e14122a0a8
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-04 00:12:42 -0500

    (6 Kyu) find-the-missing-letter.js

---
+++ javascript/6-kyu/find-the-missing-letter.js
@@ -0,0 +1,49 @@
+// Name
+/*
+  Find the missing letter
+*/
+
+// Url
+/*
+  https://www.codewars.com/kata/find-the-missing-letter/train/javascript
+*/
+
+// Instructions
+/*
+  Write a method that takes an array of consecutive (increasing) 
+  letters as input and that returns the missing letter in the array.
+
+  You will always get an valid array. And it will be always exactly 
+  one letter be missing. The length of the array will always be at least 2.
+
+  The array will always contain letters in only one case.
+
+  Example:
+
+  ['a','b','c','d','f'] -> 'e'
+  ['O','Q','R','S'] -> 'P'
+  (Use the English alphabet with 26 letters!)
+
+  Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+  I have also created other katas. Take a look if you enjoyed this kata!
+*/
+
+function findMissingLetter(array) {
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (i !== array.length) {
+      if (array[i + 1].charCodeAt(0) - array[i].charCodeAt(0) === 1) {
+        newArray.push(array[i]);
+      } else {
+        newArray.push(array[i], String.fromCharCode(array[i] + 1));
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+
+  return newArray;
+}
+findMissingLetter(['a', 'b', 'c', 'd', 'f']);
