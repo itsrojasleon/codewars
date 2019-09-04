41e51e6303631da48728d05449b7b68fb32c37f9
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-04 01:11:21 -0500

    (8 Kyu) find-numbers-which-are-divisible-by-given-number.js

--- 8-kyu/find-numbers-which-are-divisible-by-given-number.js
+++ 8-kyu/find-numbers-which-are-divisible-by-given-number.js
@@ -1,11 +1,18 @@
+// Name
+/*
+  Find numbers which are divisible by given number
+*/
+
 // Url
 /*
-  https
+  https://www.codewars.com/kata/find-numbers-which-are-divisible-by-given-number/train/javascript
 */
 
 // Instructions
 /*
-  Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+  Complete the function which takes two arguments and returns 
+  all numbers which are divisible by the given divisor. 
+  First argument is an array of numbers and the second is the divisor.
 
   Example
   divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
