ff013f2be12adac97f4d86d240fc71087eac2d29
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-07 12:13:41 -0500

    (8 Kyu) logical-calculator.js

---
+++ 8-kyu/logical-calculator.js
@@ -0,0 +1,63 @@
+// Name
+/*
+  Logical calculator
+*/
+
+// Url
+/*
+  https://www.codewars.com/kata/logical-calculator/train/javascript
+*/
+
+// Instructions
+/*
+  Your task is to calculate logical value of boolean array. 
+  Test arrays are one-dimensional and their size is in the range 1-50.
+
+  Links referring to logical operations: AND, OR and XOR.
+
+  You should begin at the first value, 
+  and repeatedly apply the logical operation across the remaining elements 
+  in the array sequentially.
+
+  First Example:
+
+  Input: true, true, false, operator: AND
+
+  Steps: true AND true -> true, true AND false -> false
+
+  Output: false
+
+  Second Example:
+
+  Input: true, true, false, operator: OR
+
+  Steps: true OR true -> true, true OR false -> true
+
+  Output: true
+
+  Third Example:
+
+  Input: true, true, false, operator: XOR
+
+  Steps: true XOR true -> false, false XOR false -> false
+
+  Output: false
+
+  Input:
+
+  boolean array, string with operator' s name: 'AND', 'OR', 'XOR'.
+
+  Output:
+
+  calculated boolean
+*/
+
+function logicalCalc(array, op) {
+  const operations = {
+    AND: (a, b) => a && b,
+    OR: (a, b) => a || b,
+    XOR: (a, b) => a !== b
+  };
+  return array.reduce(operations[op]);
+}
+logicalCalc([true, true, true, false], 'AND');
