5968ec8f4502067da52d16327711c6cac8ea605f
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-13 01:56:59 -0500

    (8 Kyu) makeuppercase.js

---
+++ 8-kyu/makeuppercase.js
@@ -0,0 +1,19 @@
+// Name
+/*
+  MakeUpperCase
+*/
+
+// Url
+/*
+  https://www.codewars.com/kata/makeuppercase/train/javascript
+*/
+
+// Instructions
+/*
+  Write function makeUpperCase.
+*/
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+makeUpperCase('hello');
