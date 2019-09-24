fabe520e2b14cde1fb63781b9145d6f275925626
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-24 12:14:36 -0500

    (8 Kyu) grasshopper-check-for-factor.js

---
+++ 8-kyu/grasshopper-check-for-factor.js
@@ -0,0 +1,34 @@
+// Name
+/*
+  Grasshopper - Check for factor
+*/
+
+// Url
+/*
+  https://www.codewars.com/kata/grasshopper-check-for-factor/train/javascript
+*/
+
+// Instructions
+/*
+  This function should test if the factor is a factor of base.
+
+  Return true if it is a factor or false if it is not.
+
+  Information
+  Factors are numbers you can multiply together to get another number.
+
+  2 and 3 are factors of 6 because:
+
+  2 * 3 = 6
+  You can find a factor by dividing numbers. If the remainder is 0 then the 
+  number is a factor.
+  In Javascript and C# you can use % to check for a remainder
+  For example 2 is not a factor of 7 because:
+
+  7 % 2 = 1
+*/
+
+function checkForFactor(base, factor) {
+  return base % factor === 0;
+}
+checkForFactor(10, 2);
