75032c2a2e0f7c69a01ca29facb339e7dde90a44
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-23 14:45:40 -0500

    (7 Kyu) the-highest-profit-wins.js

---
+++ 7-kyu/the-highest-profit-wins.js
@@ -0,0 +1,29 @@
+// Name
+/*
+  The highest profit wins!
+*/
+
+// Url
+/*
+  https://www.codewars.com/kata/the-highest-profit-wins/train/javascript
+*/
+
+// Instructions
+/*
+  Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+  Task
+  Write a function that returns both the minimum and maximum number of the given list/array.
+
+  Examples
+  minMax([1,2,3,4,5])   == [1,5]
+  minMax([2334454,5])   == [5, 2334454]
+  minMax([1])           == [1, 1]
+  Remarks
+  All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+*/
+
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}
+minMax([1, 2, 3, 4, 5]);
