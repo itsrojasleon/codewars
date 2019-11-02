fba4a5ea49e4ccf3673923c7d1e059243128f83b
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-11-02 16:50:47 -0600

    (8 Kyu) swap-values.js

---
+++ 8-kyu/swap-values.js
@@ -0,0 +1,22 @@
+// Name
+/*
+  Swap Values
+*/
+
+// Url
+/*
+  https://www.codewars.com/kata/swap-values/train/javascript
+*/
+
+// Instructions
+/*
+  I would like to be able to pass an array with two elements to my swapValues 
+  function to swap the values. However it appears that the values aren't changing.
+
+  Can you figure out what's wrong here?
+*/
+
+function swapValues(arr) {
+  return arr.reverse();
+}
+swapValues([1, 2]);
