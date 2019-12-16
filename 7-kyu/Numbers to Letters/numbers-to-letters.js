3f3ab93e2ac6008d6fbfab88c5a24a4e28019367
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-12-15 20:31:26 -0600

    Better syntax

--- 7-kyu/Numbers to Letters/numbers-to-letters.js
+++ 7-kyu/Numbers to Letters/numbers-to-letters.js
@@ -14,17 +14,30 @@
 */
 
 function switcher(x) {
+  // let result = '';
+
+  // for (let char of x) {
+  //   if (Number(char) <= 26) {
+  //     result += String.fromCharCode(123 - char);
+  //   } else {
+  //     if (char === '27') result += '!';
+  //     else if (char === '28') result += '?';
+  //     else result += ' ';
+  //   }
+  // }
+  // return result;
+
   let result = '';
 
-  for (let char of x) {
-    if (Number(char) <= 26) {
-      result += String.fromCharCode(123 - char);
-    } else {
-      if (char === '27') result += '!';
-      else if (char === '28') result += '?';
-      else result += ' ';
-    }
-  }
+  x.map(str => {
+    str === '27'
+      ? (result += '!')
+      : str === '28'
+      ? (result += '?')
+      : str === '29'
+      ? (result += ' ')
+      : (result += String.fromCharCode(123 - str));
+  });
   return result;
 }
 // switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4'])
