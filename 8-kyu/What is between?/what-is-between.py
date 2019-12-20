fc59481ca00c55e9297da9726612f2e53883a81a
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-12-20 12:45:16 -0600

    change

--- 8-kyu/What is between?/what-is-between.py
+++ 8-kyu/What is between?/what-is-between.py
@@ -1,3 +1,9 @@
+# What is between?
+
+# https://www.codewars.com/kata/what-is-between/train/python
+
+# Write a function taking two parameters and returning an int array of all elements between the input parameters and including them.
+
 def between(a,b):
   result = []
   for i in range(a, b + 1):
