3928edd5813afffa13cfbaeb2615a3a2d3697e56
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-01-13 13:53:45 -0600

    (8 Kyu) Incorrect division method)

---
+++ 8-kyu/Incorrect division method/incorrect-division-method.py
@@ -0,0 +1,9 @@
+# Name
+#   Incorrect division method
+# Url
+#   https://www.codewars.com/kata/54d1c59aba326343c80000e7/train/python
+# Instructions
+#   This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+def divide_numbers(x,y):
+    return x / float(y)
