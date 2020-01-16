8a336e232ab044e6228d2ca06d618c309955b4a1
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-01-15 20:13:07 -0600

    (8 Kyu) you-cannot-code-under-pressure-1

---
+++ 8-kyu/You Can't Code Under Pressure #1/you-ca-not-code-under-pressure-#1.py
@@ -0,0 +1,9 @@
+# Name
+  # You Can't Code Under Pressure #1
+# Url
+  # https://www.codewars.com/kata/53ee5429ba190077850011d4/train/python
+# Instructions
+  # Code as fast as you can! You need to double the integer and return it.
+
+def double_integer(i):
+  return i * 2
